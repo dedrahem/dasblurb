2fd304fd125073439075d8a61b2eb23ef0fb2ed4
douglas drahem <drahemde@gmail.com>
2016-05-03 17:54:07 -0500

    started creating authenticate, need to create sessions controller next step.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+def authenticate_user!
+  if @current_user.blank?
+    redirect_to sign_in_path
+  end
 end
