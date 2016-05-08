79c6df50742ff41937b3183d428079e26ea9c4c0
douglas drahem <drahemde@gmail.com>
2016-05-07 23:54:27 -0500

    added faker gem data doesn't import

--- Gemfile
+++ Gemfile
@@ -49,7 +49,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
+gem 'faker', '~> 1.6', '>= 1.6.3'
 gem 'travis', '~> 1.8', '>= 1.8.2'
 gem 'minitest-rails'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
+require 'csv'
+require 'faker'
+
 class UsersController < ApplicationController
+
+    def list
+      @users = fetch_users
+
+      if params[:search_text].present?
+        @users = @users.select do |user|
+          user.user_name.downcase.include? params[:search_text].downcase
+        end
+      end
+    end
+
+    def view
+      @user = fetch_user.find{|user| user.id == params[:id].to_i }
+    end
+
+    def fetch_users
+      id = 1
+      users = []
+      CSV.foreach("users.csv", headers: true) do |row|
+        user_hash = row.to_hash
+
+        user = User.new
+        user.id = id
+        user.user_name = user_hash["User Name"]
+        user.email = user_hash["Email"]
+        user.password_digest = user_hash["User Password"]
+        user.created_at = user_hash["created at"]
+        user.updated_at = user_hash["updated at"]
+        users << user
+        id += 1
+      end
+
+      # sort bu user_name
+      users = users.sort {|x| x.user_name }
+
+    end
+
+
   def index
   end
 
@@ -29,4 +70,5 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
 end
