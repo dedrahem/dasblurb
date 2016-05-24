18e0e28c399548d7b1c352cb812dd9590286f006
douglas drahem <drahemde@gmail.com>
2016-05-23 21:05:35 -0500

    first working seed file created using faker for posts and users, . .

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,18 @@ Post.delete_all
 if User.count == 0
 	usercounter = 0
 	until usercounter == 24
-		User.create(user_name: Faker::Name.name,
+		user = User.create(user_name: Faker::Name.name,
                 	email: Faker::Internet.email,
                 	password: Faker::Internet.password(8, 10),
                 	created_at: Faker::Time.between(DateTime.now - 7, DateTime.now - 5),
 			            updated_at: Faker::Time.between(DateTime.now - 4, DateTime.now))
+		Post.create(postbody: Faker::Hipster.sentence,
+									user_id: user.id,
+									created_at: Faker::Time.between(DateTime.now - 7, DateTime.now - 5),
+									updated_at: Faker::Time.between(DateTime.now - 4, DateTime.now))
   usercounter = usercounter + 1  #  selects the number of users that are created
 	end
 end
 puts "------------------------------------------------------------"
-puts "-              User create section completed               -"
-puts "------------------------------------------------------------"
-if Post.count == 0
-	postcounter = 0
-	until postcounter == 24
-		Post.create(postbody: Faker::Hipster.sentence,
-	                	user_id: user_name[postcounter].id,
-	                	created_at: Faker::Time.between(DateTime.now - 7, DateTime.now - 5),
-				            updated_at: Faker::Time.between(DateTime.now - 4, DateTime.now))
-	postcounter = postcounter + 1
-	end
-end
-puts "------------------------------------------------------------"
-puts "-             Post  creat  complete                        -"
+puts "-              User and Post created                       -"
 puts "------------------------------------------------------------"
