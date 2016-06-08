67a1aa205fd86dc5f27b2ae2fbac61dee3ddf505
douglas drahem <drahemde@gmail.com>
2016-06-08 11:39:42 -0500

    adding uniqueness for emails and validations for addresses ref 256, ......

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :user_name, presence: true
-  validates :email, presence: true
+  validates :user_name, presence: true, length: {maximum: 48}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_many :posts
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
-
+#  / because there is a production Vs a test database  /
+#  /  fixtures or hardcode    /
+#  /  def setup              /
+#  /  @user = User.find(1)  /
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(user_name: "Mike Vapor", email: "mike@bigcity.com", password_digest: "1234")
+    @user = User.new(user_name: "Mike Vapor", email: "mike@bigcity.com", password: "1234")
+    @user.save!
   end
 
   test "user model should be valid" do
@@ -15,14 +19,51 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email should be populated" do
+  test "= email should be populated =" do
     @user.email = "   "
   assert_not @user.valid?
   end
 
-end
+#  / constraints are set in  app/models/user.rb !! //
+#  / creates a user name of 49 chars in length  /
+  test "..user_name to be 48 char max.." do
+    @user.user_name = "x" * 49
+    assert_not @user.valid?
+  end
 
-#  / because there is a production Vs a test database  /
-#  /  fixtures or hardcode    /
-#  /  def setup              /
-#  /  @user = User.find(1)  /
+#  / let email length be 255 max  /
+#  / example email string made to be 256 char in length /
+#  / 244 + 12 = 256 /
+  test "< email shall be a max of 255 >" do
+    @user.email = "a" * 244 + "@bigcity.com"
+    assert_not @user.valid?
+  end
+
+  test ".. email validation should work and recognize valid addresses" do
+    valid_addresses = %w[user@example.com mike@bigcity.com first.last@none.xp mae_moriette@bashirianemard.org camylle@balistreri.name isai@brown.org antonina@connelly.name cristian_collier@ondrickaleannon.com felix@herzognicolas.org jasmin.towne@baileyheel.io britney_beatty@crist.name rylan.fadel@marksbarton.co alice+bob@baz.cn A_ERADO-j@foo.bar.org cindycash@caddy.net dr.moriarity@gentlemen.net]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # / these addresses are causing the test to fail /
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test ".. email validation rejecting bogus or suspect address ..." do
+    invalid_addresses = %w[user@example,com cindycash$@dollar.net mikeatbigcity.com user_at_first.com mae_moriette@bash. camylle@bal_bal.org antonial@buzz+beer.com jakethebear uncle.fester@]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      # / these addresses are causing the test to fail /
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "-< email addresses should be unique ...>-" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+end
